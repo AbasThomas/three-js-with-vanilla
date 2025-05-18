c51fa6a217b860338ab72d116cd2ef3bed83af37
Thomas Abas <abasienyenethomas2@gmail.com>
2025-05-17 21:09:09 -0800

    making changes to app.jsx

--- src/main.js
+++ src/main.js
@@ -1,24 +1,36 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import { color, Const } from 'three/tsl';
+import { Mesh } from 'three/webgpu';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const canvas:HTMLElement = document.getElementById(elementId: 'canvas');
+// 1 Create a scene object
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0');
 
-setupCounter(document.querySelector('#counter'))
+// 2 Create a camera object
+const camera = new THREE.PerspectiveCamera(fov:75,aspect: window.innerWidth / window.innerHeight, near:0.1, far: 1000);
+camera.position.z = 5;
+
+// 3 Create a renderer object
+
+const geometry = new THREE.DodecahedronGeometry();
+const material = new THREE.MeshLambertMaterial({ color: '#468585', emissive: '#468585' });
+const dodecahedron = new THREE.Mesh(geometry, material);
+
+const boxGeometry = new THREE.BoxGeometry(width = 2, height = 0.1, depth = 2);
+const boxMaterial = new THREE.MeshLambertMaterial({ color: '#b4b4b3',  });
+
+ const box:Mesh = new THREE.Mesh(boxGeometry, boxMaterial);
+ box.position.y = -1.5
+ scene.add(dodecahedron)
+ scene.add(box)
+
+ // 4 Create a light source
+const light = new THREE.SpotLight(color:0x006769, intensity:100 );
+light.position.set(x : 1, y: 1, z: 1);
+  scene.add(light);
+
+// 5 Create a WebGL renderer
+const renderer = new THREE.WebGLRenderer( parameters: { canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
